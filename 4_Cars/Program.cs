1812549085dd42a635532cdbce974d713de9cf26
PiotrPal <133272105+PiotrPal@users.noreply.github.com>
2024-04-30 19:26:04 +0200

    wczytywanie csv

---
+++ 4_Cars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Cars {
+    internal class Program {
+        static void Main(string[] args) {
+            var samochody = WczytywanieZpliku("paliwo.csv");
+
+            foreach (var car in samochody) {
+                //Console.WriteLine(car.Rok);
+                Console.WriteLine(car.Producent + " " + car.Model);
+            }
+        }
+
+        private static List<Samochod> WczytywanieZpliku(string path) {
+            return File.ReadAllLines(path)
+                .Skip(1)
+                .Where(line => line.Length > 1)
+                .Select(Samochod.ParseCSV).ToList();
+        }
+
+    }
+}
